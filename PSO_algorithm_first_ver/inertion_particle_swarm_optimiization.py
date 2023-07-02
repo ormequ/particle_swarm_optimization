e2dfd3eb208406eb8bddcd1c7b8731fcc53498f1
MuhachevaPolina <muhachevapolina@icloud.com>
2023-07-03 00:31:13 +0300

    changes ver.2 for PSO algorithm

--- PSO_algorithm_first_ver/inertion_particle_swarm_optimiization.py
+++ PSO_algorithm_first_ver/inertion_particle_swarm_optimiization.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class Plane_Function:
+class PlaneFunction:
     # test
     # z = Ax + By + C
     def __init__(self, A, B, C):
@@ -9,7 +9,6 @@ class Plane_Function:
         self.C = C
     def __call__(self, x, y):
         return self.A * x + self.B * y + self.C
-
 
 
 class Particle:
@@ -21,14 +20,13 @@ class Particle:
 
 
 class Swarm:
-    def __init__(self, num_of_particles, inertion, weight1, weight2, boundaries_x, boundaries_y, function):
+    def __init__(self, num_of_particles, inertion, local_weight, global_weight, boundaries, function):
         self.function = function
         self.num_of_particles = num_of_particles
         self.inertion = inertion
-        self.weight1 = weight1
-        self.weight2 = weight2
-        self.boundaries_x = boundaries_x
-        self.boundaries_y = boundaries_y
+        self.local_weight = local_weight
+        self.global_weight = global_weight
+        self.boundaries = boundaries
         self.best_global_pos = None
         self.best_global_val = None
 
@@ -41,21 +39,21 @@ class Swarm:
     def create_particles(self):
         '''
         равномерное распределение частиц
-        присваение минимального значения для каждой частицы
+        присваивание минимального значения для каждой частицы
         поиск минимального значения среди них
         '''
         self.particles = list()
 
         for i in range(self.num_of_particles):
-            tmp_pos = self.gen_rand_vector(self.boundaries_x, self.boundaries_y)
-            cur_position_value = self.function(tmp_pos[0], tmp_pos[1])
+            start_pos = self.gen_rand_vector(self.boundaries[0], boundaries[1])
+            cur_position_value = self.function(start_pos[0], start_pos[1])
             velocity_boundaries = lambda boundaries: [- (boundaries[1] - boundaries[0]), boundaries[1] - boundaries[0]]
-            tmp_vel = self.gen_rand_vector(velocity_boundaries(self.boundaries_x), velocity_boundaries(self.boundaries_y))
-            self.particles.append(Particle(tmp_pos, tmp_vel))
+            start_vel = self.gen_rand_vector(velocity_boundaries(self.boundaries[0]), velocity_boundaries(self.boundaries[1]))
+            self.particles.append(Particle(start_pos, start_vel))
             self.particles[-1].best_value = cur_position_value
 
             if i == 0 or cur_position_value < self.best_global_val:
-                self.best_global_pos = tmp_pos.copy()
+                self.best_global_pos = start_pos.copy()
                 self.best_global_val = cur_position_value
 
     def update_particle_state(self, index):
@@ -64,17 +62,24 @@ class Swarm:
         передвижение частицы, если позволяет диапазон
         '''
         cur_particle = self.particles[index]
-        self.particles[index].velocity = self.inertion * cur_particle.velocity + weight1 * np.random.rand(2) * (cur_particle.best_pos - cur_particle.position) + weight2 * np.random.rand(2) * (self.best_global_pos - cur_particle.position)
+        velocity_component = self.inertion * cur_particle.velocity
+        local_component = local_weight * np.random.rand(2) * (cur_particle.best_pos - cur_particle.position)
+        global_component = global_weight * np.random.rand(2) * (self.best_global_pos - cur_particle.position)
+        self.particles[index].velocity = velocity_component + local_component + global_component
 
         new_position = cur_particle.position + self.particles[index].velocity
-        if self.boundaries_x[0] <= new_position[0] and new_position[0] <= self.boundaries_x[1]: # проверка: координаты частицы не вылезли за значение по Х
-            if self.boundaries_y[0] <= new_position[1] and new_position[1] <= self.boundaries_y[1]: # проверка: координаты частицы не вылезли за значение по Y
-                self.particles[index].position = new_position
-                new_value = self.function(self.particles[index].position[0], self.particles[index].position[1])
-                if new_value < cur_particle.best_value:
-                    self.particles[index].best_value = new_value
-                    self.particles[index].best_pos = self.particles[index].position.copy()
-                # тут, если скорость слишком большая/маленькая, чтобы двигаться дальше, можно точку просто на границу, наверное, ставить
+        for i in range(len(self.boundaries)):
+            if self.boundaries[i][0] >= new_position[i]: # проверка: координаты частицы не меньше значения левой границы по Х либо Y
+                new_position[i] = self.boundaries[i][0]
+            if new_position[i] >= self.boundaries[i][1]: # проверка: координаты частицы не больше значения правой границы по Х либо Y
+                new_position[i] = self.boundaries[i][1]
+
+        self.particles[index].position = new_position
+
+        new_value = self.function(self.particles[index].position[0], self.particles[index].position[1])
+        if new_value < cur_particle.best_value:
+            self.particles[index].best_value = new_value
+            self.particles[index].best_pos = self.particles[index].position.copy()
 
     def next_iteration(self):
         '''
@@ -94,15 +99,16 @@ class Swarm:
 num_of_iterations = int(input("Введите количество итераций: "))
 num_of_particles = int(input("Введите количество частиц: "))
 inertion = float(input("Введите инерцию: "))
-weight1 = float(input("Введите первый параметр: "))
-weight2 = float(input("Введите второй параметр: "))
+local_weight = float(input("Введите локальный параметр: "))
+global_weight = float(input("Введите глобальный параметр: "))
 boundary_x_lower = float(input("Введите ограничение по Х снизу: "))
 boundary_x_upper = float(input("Введите ограничение по Х сверху: "))
 boundary_y_lower = float(input("Введите ограничение по Y снизу: "))
 boundary_y_upper = float(input("Введите ограничение по Y сверху: "))
 
-function = Plane_Function(1, 2, 3)
-swarm = Swarm(num_of_particles, inertion, weight1, weight2, [boundary_x_lower, boundary_x_upper], [boundary_y_lower, boundary_y_upper], function)
+function = PlaneFunction(1, 2, 3)
+boundaries = np.array([[boundary_x_lower, boundary_x_upper], [boundary_y_lower, boundary_y_upper]])
+swarm = Swarm(num_of_particles, inertion, local_weight, global_weight, boundaries, function)
 swarm.create_particles()
 
 for i in range(num_of_iterations):
